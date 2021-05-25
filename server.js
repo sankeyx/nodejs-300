9af4ee48ee7d8fa8691343b5ac60d84493e6bfdd
sanketgcoutloot <sanketgulavani@coutloot.com>
2021-05-25 13:27:12 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send(' Node.js app server 1 3001!')
+})
+
+app.listen(3001, () => {
+    console.log('Server is up on 3001')
+})
